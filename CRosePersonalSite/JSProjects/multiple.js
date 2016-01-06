e0673a4d42f96e17ddeb6deab2c9a50773cfe68a
Chris Rose <cdrose15@gmail.com>
2016-01-06 15:05:32 -0500

    First upload
    First upload

---
+++ CRosePersonalSite/JSProjects/multiple.js
@@ -0,0 +1,24 @@
+﻿function getMultiple() {
+
+    var x = document.getElementById("a").value
+    var y = document.getElementById("b").value
+    document.getElementById("mult").innerHTML = multiple(x, y);
+}
+
+function multiple(x,y) {
+    for (i=1; i<=100; i++){
+
+         if (i % x == 0 && i % y == 0){
+             document.write ("FizzBuzz" + "-")
+         }
+        else if (i % x == 0) {
+             document.write  ("Fizz" + "-")
+        }
+        else if (i % y == 0) {
+            document.write ("Buzz" + "-")
+        }
+        else{
+             document.write ( i + "-");
+        }    
+    }
+}
