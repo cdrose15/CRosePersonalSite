f378842c8d41a226892a7a56271b3d3d3f291c43
Chris Rose <cdrose15@gmail.com>
2016-01-16 17:49:01 -0500

    1/16/16
    changed about me pic

--- CRosePersonalSite/obj/Release/Package/PackageTmp/JSProjects/calculations.js
+++ CRosePersonalSite/obj/Release/Package/PackageTmp/JSProjects/calculations.js
@@ -7,16 +7,20 @@
          n4 = parseInt($("#num4").val(), 10);
          n5 = parseInt($("#num5").val(), 10);
 
-         a = (n1 + n2 + n3 + n4 + n5);
-         b = (n1 * n2 * n3 * n4 * n5);
-         c = ((n1 + n2 + n3 + n4 + n5) /5);
-         d = Math.max(n1, n2, n3, n4, n5);
-         e = Math.min(n1, n2, n3, n4, n5);
+         if (n1 == null, n1 == "") (n2 == null, n2 == "")(n3 == null , n3 == "")
+          (n4 == null , n4 == "")(n5 == null , n5 == "");{
 
-        $("#sum").text("The sum = " + a);
-        $("#product").text("The product = " + b);
-        $("#mean").text("The mean = " + c);
-        $("#max").text("The maximum value = " + d);
-        $("#min").text("The minimum value = " + e);
+              a = (n1 + n2 + n3 + n4 + n5);
+              b = (n1 * n2 * n3 * n4 * n5);
+              c = ((n1 + n2 + n3 + n4 + n5) /5);
+              d = Math.max(n1, n2, n3, n4, n5);
+              e = Math.min(n1, n2, n3, n4, n5);
+
+              $("#sum").text("The sum = " + a);
+              $("#product").text("The product = " + b);
+              $("#mean").text("The mean = " + c);
+              $("#max").text("The maximum value = " + d);
+              $("#min").text("The minimum value = " + e);
+          }
     });
 });
